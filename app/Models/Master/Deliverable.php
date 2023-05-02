f252b9e03d48cb2cf627f6acc1ec6e0ad00111ac
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-05-02 14:45:30 +0530

    Master Table releation ship added (belogs to)

--- app/Models/Master/Deliverable.php
+++ app/Models/Master/Deliverable.php
@@ -4,8 +4,13 @@ namespace App\Models\Master;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Deliverable extends Model
 {
     use HasFactory;
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by_user_id', 'id');
+    }
 }
--- app/Models/Master/Team.php
+++ app/Models/Master/Team.php
@@ -2,10 +2,16 @@
 
 namespace App\Models\Master;
 
+use App\Models\User\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Team extends Model
 {
     use HasFactory;
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by_user_id', 'id');
+    }
 }
