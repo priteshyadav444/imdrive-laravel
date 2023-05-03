209b07f438f17880430cf3e73ad5827874bc4c8f
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-05-03 13:01:06 +0530

    factories added for master table - Deliverable - Team

--- database/factories/Master/DeliverableFactory.php
+++ database/factories/Master/DeliverableFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories\Master;
 
+use App\Models\User\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class DeliverableFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'user_id' => $this->faker->randomElement(User::pluck('id'))
         ];
     }
 }
