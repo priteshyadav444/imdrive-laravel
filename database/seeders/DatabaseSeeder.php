912499cc4cb52f6de5dc91e74b63a1ee0606a208
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-05-03 13:01:53 +0530

    Seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,15 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Master\Deliverable;
+use App\Models\Master\Team;
+use App\Models\Project\Project;
+use App\Models\User\User;
+use Database\Factories\Project\ProjectFactory;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +20,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User\User::factory(1)->create();
+        User::factory(100)->create();
+        Team::factory(5)->create();
+        Deliverable::factory(5)->create();
+        Project::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
