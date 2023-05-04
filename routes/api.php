a82822e9189591166699a4656bfbff4c41b0e243
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-05-04 18:52:33 +0530

    route updated

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/users', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::prefix("users")->group(function () {
+    Route::get("/", [UserController::class, 'index']);
+    Route::get("/{id}", [UserController::class, 'show']);
+    Route::post("/", [UserController::class, 'store']);
+    Route::put("/{id}", [UserController::class, 'update']);
+    Route::patch("/{id}", [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
 });
+
+// Route::apiResource("users", UserController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Models\Master\Deliverable;
+use App\Models\Master\Team;
+use App\Models\Project\Project;
+use App\Models\User\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +18,67 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+    // $teams = User::find(2)->createdTeams->toArray();
+    // dd($teams);
+    // foreach ($teams as $team) {
+    //     dd($team);
+    // }
+    // $user = Team::find(4)->createdBy->toArray();
+
+    // $team = new Team();
+    // $teamWithUser = $team->where('id', 4)->with("createdBy")->get()->toArray();
+
+    // $project = Project::where('id', 5)->with('deliverables')->get()->first();
+    // $project = Deliverable::where('id', 1)->with('deliverables')->get()->toArray();
+    // dd($project->deliverables->first()->toArray()['project_deliverables']['id']);
+
+
+    // $users  = User::find(1)->toArray();
+    // $users = new User();
+    // $users = User::get();
+    // $emails = $users->pluck('email')->all();
+    // dd($users->pluck('email')->all());
+
+    // $updatedEmails = $users->each(function ($item) {
+    //     $item->is_verified = true;
+    // });
+
+
+    // $inActiveUsers = $users->filter(function ($item) {
+    //     return $item->account_status == "inactive";
+    // });
+
+    // dd($inActiveUsers->toArray());
+
+    // $todocollection = collect([
+    //     [
+    //         'user_id' => '1',
+    //         'title' => 'Do Laundry',
+    //         'description' => 'I have to do laundry '
+    //     ],
+    //     [
+    //         'user_id' => '3',
+    //         'title' => 'Finish Assignment',
+    //         'description' => 'I have to finish Maths assignment '
+    //     ],
+    // ]);
+
+    // dd($todocollection->where('user_id', 1)->all());
+
+    // dd($users->contains('email', 'mquigley1@example.org'));
+
+
+    // $projectCreated = User::find(92)->createdProjects->toArray();
+
+    // $user = Project::find(1)->users->get()->toArray();
+
+    // $user = Project::with('users')->find(1)->toArray();
+
+    // dd(DB::last_query());
+
+
+//     $user = User::OfType()->get()->toArray();
+//     dd($user);
+//     return $user;
+// });
